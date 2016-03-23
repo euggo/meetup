3e8252b77298e9e4cca6256d091fe7abdbf85729
Daved <daved@codemodus.com>
2016-03-23 16:13:47 -0700

    Remove useles type conversion.

--- _x/talks/mini_api_mini_rpc/e_add_node_http-server/node_h.go
+++ _x/talks/mini_api_mini_rpc/e_add_node_http-server/node_h.go
@@ -21,7 +21,7 @@ func (n *node) localHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte(b))
+	w.Write(b)
 }
 
 func (n *node) remoteHandler(w http.ResponseWriter, r *http.Request) {
