a1c4cefc84bf523e3185f9eca7db97432cf2c2c2
Daved <daved@codemodus.com>
2016-11-03 17:06:39 -0700

    Rename concurrency talk, move to live dir.

---
+++ talks/feeding_concurrent_processes/conch0/conch.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"errors"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+// conch holds a file info group, and done, out, and err channels.
+//START1 OMIT
+type conch struct {
+	fig  *fileInfoGroup
+	done chan struct{}
+	out  chan *fileOutput
+	err  chan error
+}
+
+//END1 OMIT
+
+// newConch returns a pointer to a new conch with channels setup.
+//START2 OMIT
+func newConch(fig *fileInfoGroup) *conch {
+	return &conch{
+		fig:  fig,
+		done: make(chan struct{}),
+		out:  make(chan *fileOutput),
+		err:  make(chan error, 1),
+	}
+}
+
+//END2 OMIT
+
+// doneChan returns the conch done channel.
+//START3 OMIT
+func (c *conch) doneChan() chan struct{} {
+	return c.done
+}
+
+//END3 OMIT
+
+// feedPaths is a generator that sends paths out for processing. If canceled,
+// an error is returned.
+//START5 OMIT
+func (c *conch) feedPaths(paths chan string) {
+	for _, v := range c.fig.fsi {
+		select {
+		case paths <- filepath.Join(c.fig.dir, v.Name()):
+		case <-c.done:
+			c.err <- errors.New("canceled")
+		}
+	}
+
+	close(paths)
+}
+
+//END5 OMIT
+
+// digest processes the file located at the currently provided path, and
+// sends out a new result.
+//START7 OMIT
+func (c *conch) digest(paths <-chan string) {
+	for p := range paths {
+		r := newFileOutput(p)
+
+		if slow {
+			select {
+			case <-time.After(time.Second):
+			case <-c.done:
+				return
+			}
+		}
+		//OMIT
+		select {
+		case c.out <- r:
+		case <-c.done:
+			return
+		}
+	}
+}
+
+//END7 OMIT
+
+// fanout sets-up digest goroutines according to width. Each digest goroutine
+// waits for data from the paths channel and the entire function collapses when
+// completed.
+//START6 OMIT
+func (c *conch) fanout(paths chan string) {
+	var wg sync.WaitGroup
+	wg.Add(width)
+
+	// setup digesters by width
+	for i := 0; i < width; i++ {
+		go func() {
+			c.digest(paths)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	close(c.out)
+}
+
+//END6 OMIT
+
+// run wires up the paths chan, calls the path generator, calls the fanout, and
+// returns the out and err channels.
+//START4 OMIT
+func (c *conch) run() (<-chan *fileOutput, <-chan error) {
+	paths := make(chan string)
+
+	go c.feedPaths(paths)
+	go c.fanout(paths)
+
+	return c.out, c.err
+}
+
+//END4 OMIT
---
+++ talks/feeding_concurrent_processes/conch0/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+//START1 OMIT
+var (
+	// slow enables a slowing of the digest function and may help users to // OMIT
+	// better understand the implementation of concurrency // OMIT
+	slow = false
+	// OMIT
+	// width controls the amount of goroutines running the digest function // OMIT
+	width = 8
+)
+
+//END1 OMIT
+
+//START2 OMIT
+func main() {
+	// define and parse flags // OMIT
+	flag.BoolVar(&slow, "slow", slow, `slow processing to clarify behavior`)
+	flag.IntVar(&width, "width", width, `set concurrency width`)
+	flag.Parse()
+	//END2 OMIT
+
+	// get fileInfoGroup
+	//START3 OMIT
+	fig, err := newFileInfoGroup("./testfiles")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	//END3 OMIT
+
+	// get new conch and setup cleanup
+	//START4 OMIT
+	c := newConch(fig)
+	defer close(c.doneChan())
+
+	// get fileOutput and error channels // OMIT
+	fos, errc := c.run()
+	//END4 OMIT
+
+	// print file contents
+	//START5 OMIT
+	for fo := range fos {
+		fmt.Println(fo.path, fo.data, fo.err)
+	}
+
+	// print error, if any
+	select {
+	case err := <-errc:
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	default:
+	}
+}
+
+//END5 OMIT
