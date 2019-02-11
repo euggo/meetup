652ae6fc2de0171bf5f2030c91d0b144088fb937
Daved <daved@codemodus.com>
2019-02-11 13:10:34 -0800

    Update inherit pres

---
+++ talks/objectively_harmful/code/js/h_composition.js
@@ -0,0 +1,45 @@
+function meet(name, greeters) {
+  for (var i = 0; i < greeters.length; i++) {
+    console.log(greeters[i].greeting(name));
+  };
+}
+
+function wrappedMeet(name, greeters) {
+  for (var i = 0; i < greeters.length; i++) {
+    console.log(greeters[i].wrappedGreeting(name));
+  };
+}
+
+// BGN1 OMIT
+function Human(name) {
+  this.name = name;
+
+  this.greeting = function(name) {
+    return "Hello, "+name+". I'm " + this.name + ".";
+  }.bind(this);
+}
+// END1 OMIT
+
+function Werewolf(name) {
+  this.human = new Human(name);
+
+  Object.assign(this, this.human);
+
+  this.wrappedGreeting = function(name) {
+    return this.human.greeting(name);
+  };
+}
+
+// BGN2 OMIT
+c = new Werewolf("Carlos");
+
+console.log("update 'human' name to 'Charlie' - call assigned, then wrapped");
+c.human.name = "Charlie";
+meet("Erin", [c]);
+wrappedMeet("Frank", [c]);
+
+console.log("update 'werewolf' name to 'Charles' - call assigned, then wrapped");
+c.name = "Charles";
+meet("Grace", [c]);
+wrappedMeet("Heidi", [c]);
+// END2 OMIT
